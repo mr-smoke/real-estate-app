10c3c920ac633f9fe76f9469759e8db0e85cd0ba
baki duman <g.s_baki_70@hotmail.com>
2024-09-28 09:42:42 +0300

    Refactor scss file structure and imports.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.scss";
+import "./sass/style.scss";
+
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
