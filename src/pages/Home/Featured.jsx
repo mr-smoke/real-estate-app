af37ca845e99bfb7df66f280b2e13750d4ee395a
baki duman <g.s_baki_70@hotmail.com>
2024-09-29 15:16:27 +0300

    Add Featured component.

---
+++ src/pages/Home/Featured.jsx
@@ -0,0 +1,35 @@
+const Featured = () => {
+  return (
+    <section className="featured">
+      <img src="./building2.jpg" alt="Featured" />
+      <h2> Buying, Selling, Renting a Home and More</h2>
+      <div className="featured__content">
+        <div className="featured__content-item">
+          <h3>Find Your Dream Home</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <a href="#">Read More</a>
+        </div>
+        <div className="featured__content-item">
+          <h3>Modern Design</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <a href="#">Read More</a>
+        </div>
+        <div className="featured__content-item">
+          <h3>Best Price Guarantee</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Featured;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Featured from "./Featured";
+import Hero from "./Hero";
+
+const Home = () => {
+  return (
+    <main>
+      <Hero />
+      <Featured />
+    </main>
+  );
+};
+
+export default Home;
