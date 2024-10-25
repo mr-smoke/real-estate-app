a14f982bb0f9634e4c12cb36fdce9e7c28f68b7a
baki duman <g.s_baki_70@hotmail.com>
2024-10-25 12:27:20 +0300

    Configuration for graphql.

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,7 +1,18 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'", 'https:'],
+          'script-src': ["'self'", 'https:'],
+          'connect-src': ["'self'", 'https:', 'http:'],
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
